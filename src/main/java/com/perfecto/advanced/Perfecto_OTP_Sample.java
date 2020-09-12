cea67f7ad4000dc46436d13a1da161be25dbd751
Genesis Thomas <gthomas@perforce.com>
2020-09-12 18:06:20 +0530

    OTP sample updates

--- src/main/java/com/perfecto/advanced/Perfecto_OTP_Sample.java
+++ src/main/java/com/perfecto/advanced/Perfecto_OTP_Sample.java
@@ -5,10 +5,13 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -53,7 +56,7 @@ public class Perfecto_OTP_Sample {
 		capabilities = new DesiredCapabilities("", "", Platform.ANY);
 		capabilities.setCapability("securityToken", Utils.fetchSecurityToken(securityToken));
 		capabilities.setCapability("enableAppiumBehavior", true);
-		capabilities.setCapability("description", "DushyantanShir");
+		capabilities.setCapability("description", "BrianGenesis");
 		capabilities.setCapability("platformName", "iOS");
 		capabilities.setCapability("openDeviceTimeout", 4);
 		capabilities.setCapability("bundleId", "com.apple.MobileSMS");
@@ -66,22 +69,27 @@ public class Perfecto_OTP_Sample {
 		}
 		String phoneNumber = Utils.getDevicePhoneNumber(driver2);
 
+		WebDriverWait wait = new WebDriverWait(driver, 60);
 		reportiumClient = Utils.setReportiumClient(driver, reportiumClient); //Creates reportiumClient
 		reportiumClient2 = Utils.setReportiumClient(driver2, reportiumClient); //Creates reportiumClient
 		reportiumClient.testStart("send SMS", new TestContext("tag1")); //Starts the reportium test
 		reportiumClient2.testStart("check SMS", new TestContext("tag2")); //Starts the reportium test
 
 		reportiumClient.stepStart("Send SMS"); //Starts a reportium step
-		WebElement action = driver.findElement(By.xpath("//*[@resource-id=\"com.samsung.android.messaging:id/fab\"]"));
-		action.isDisplayed();
-		action.click();
-		driver.findElement(By.xpath("//*[@resource-id=\"com.samsung.android.messaging:id/recipients_editor_to\"]")).sendKeys(phoneNumber);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@resource-id=\"com.samsung.android.messaging:id/fab\"]"))).click();
+		By to = By.xpath("//*[@resource-id=\"com.samsung.android.messaging:id/recipients_editor_to\"]");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(to)).click();
+		System.out.println("phone number is: "+ phoneNumber);
+		driver.findElement(to).sendKeys(phoneNumber);
 		Map<String, Object> params = new HashMap<>();
 		params.put("label", "Next");
 		driver.executeScript("mobile:button-text:click", params);
-		driver.findElement(By.xpath("//*[@resource-id=\"com.samsung.android.messaging:id/message_edit_text\"]")).isDisplayed();
-		driver.findElement(By.xpath("//*[@resource-id=\"com.samsung.android.messaging:id/message_edit_text\"]")).sendKeys(msg);
-		driver.findElement(By.xpath("//*[@resource-id=\"com.samsung.android.messaging:id/send_button1\"]")).click();
+		WebElement msgTxt = driver.findElement(By.xpath("//*[@resource-id=\"com.samsung.android.messaging:id/message_edit_text\"]"));
+		wait.until(ExpectedConditions.elementToBeClickable(msgTxt));
+		msgTxt.isDisplayed();
+		msgTxt.click();
+		msgTxt.sendKeys(msg);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@content-desc='Send' and @enabled='true']"))).click();
 		reportiumClient.stepEnd(); 
 
 		reportiumClient2.stepStart("Verify SMS in other device"); 
